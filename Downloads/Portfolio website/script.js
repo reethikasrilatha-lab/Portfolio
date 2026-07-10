f1b2fd9b7a00878926bd158188c76de8f2d90d73
Muppu Reethika <reethikasrilatha@email.com>
2026-07-10 13:21:24 +0530

    Remove fade-in animation

--- Downloads/Portfolio website/script.js
+++ Downloads/Portfolio website/script.js
@@ -70,30 +70,6 @@ document.addEventListener("DOMContentLoaded", type);
 // Fade-in Animation
 const sections = document.querySelectorAll("section");
 
-window.addEventListener("scroll", () => {
-
-    sections.forEach(section => {
-
-        const sectionTop = section.getBoundingClientRect().top;
-
-        if (sectionTop < window.innerHeight - 100) {
-
-            section.style.opacity = "1";
-            section.style.transform = "translateY(0)";
-
-        }
-
-    });
-
-});
-
-sections.forEach(section => {
-
-    section.style.opacity = "0";
-    section.style.transform = "translateY(50px)";
-    section.style.transition = "all 1s";
-
-});
 
 
 // Scroll To Top Button
